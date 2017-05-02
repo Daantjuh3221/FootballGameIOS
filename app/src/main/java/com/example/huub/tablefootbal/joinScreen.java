b8b0aeba1b8206292866baf69746b5abb50376eb
Daantjuh3221 <daanbefort@hotmail.com>
2017-05-02 09:57:55 +0200

    Updatje

--- app/src/main/java/com/example/huub/tablefootbal/joinScreen.java
+++ app/src/main/java/com/example/huub/tablefootbal/joinScreen.java
@@ -1,7 +1,9 @@
 package com.example.huub.tablefootbal;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -27,6 +29,7 @@ public class joinScreen extends AppCompatActivity {
     public String userName;
     private Socket mSocket;
     private boolean mExists = false;
+    private final SharedPreferences settings = getSharedPreferences(Constants.PREFERENCEFILENAME, Context.MODE_PRIVATE);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,6 +40,14 @@ public class joinScreen extends AppCompatActivity {
         mSocket = app.getSocket();
         mSocket.on("usernameExists", onUsernameExists);
         mSocket.connect();
+
+        if (settings.contains("username")){
+            Constants.USERNAME = settings.getString("username", "NO USERNAME");
+            mSocket.emit("connectUser", Constants.USERNAME, Constants.DEVICE, false);
+            Intent i = new Intent(getApplicationContext(), codeScreen.class);
+            startActivity(i);
+            finish();
+        }
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
@@ -45,6 +56,11 @@ public class joinScreen extends AppCompatActivity {
         public void call(final Object... args) {
                 mExists = (boolean)args[0];
                 if (!mExists) {
+                    SharedPreferences settings = getSharedPreferences(Constants.PREFERENCEFILENAME, 0);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString("username", Constants.USERNAME);
+                    // Commit the edits!
+                    editor.commit();
                     Intent i = new Intent(getApplicationContext(), codeScreen.class);
                     startActivity(i);
                     finish();
@@ -64,6 +80,6 @@ public class joinScreen extends AppCompatActivity {
     public void joinGame(View v) {
         userName = txtUserName.getText().toString();
         Constants.USERNAME = userName;
-        mSocket.emit("connectUser", Constants.USERNAME, Constants.DEVICE);
+        mSocket.emit("connectUser", Constants.USERNAME, Constants.DEVICE, true);
     }
 }
