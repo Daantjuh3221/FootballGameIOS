78a9ebbbf23be449faceb6b7a477b7aee174ce22
Daantjuh3221 <daanbefort@hotmail.com>
2017-05-30 12:33:15 +0200

    Player 2 added

--- app/src/main/java/com/example/huub/tablefootbal/joinScreen.java
+++ app/src/main/java/com/example/huub/tablefootbal/joinScreen.java
@@ -1,5 +1,7 @@
 package com.example.huub.tablefootbal;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -52,6 +55,9 @@ public class joinScreen extends AppCompatActivity implements SocketConnection.on
                 }
             }
         });
+
+
+
 
         //
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
