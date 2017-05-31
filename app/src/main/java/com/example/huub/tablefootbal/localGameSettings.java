6821b6cd7a459d4cd8d46c3e60fc65560ed233c7
jvanderlee <jellevanderlee@hotmail.nl>
2017-05-31 15:09:01 +0200

    Design

--- app/src/main/java/com/example/huub/tablefootbal/localGameSettings.java
+++ app/src/main/java/com/example/huub/tablefootbal/localGameSettings.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
